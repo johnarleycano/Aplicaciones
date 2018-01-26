49bff6f51f894254530f67e87af31a4d3d28c36a
John Arley Cano Salinas <johnarleycano@hotmail.com>
2018-01-26 14:20:11 -0500

    [1.0.0] style(general): versionamiento incorporado
    - Pié de página incorporado con versionamiento

---
+++ application/views/core/footer.php
@@ -0,0 +1,12 @@
+<footer style="
+    background-color: #808080;
+    bottom: 0;
+    color: white;
+    font-size: 0.7em;
+    left: 0;
+	position: fixed;
+    text-align: center;
+    width: 100%;
+">
+	Suite de aplicaciones | Devimed S.A. | <i>Versión 1.0.0</i>
+</footer>

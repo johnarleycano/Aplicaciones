12cc6034bcc740f1b9971d59f71170cb95519bb5
John Arley Cano <johnarleycano@hotmail.com>
2018-04-10 15:00:17 -0500

    Configuraciòn de entorno de desarrollo de mediciones

--- application/views/core/header.php
+++ application/views/core/header.php
@@ -7,7 +7,7 @@
 <!-- Scripts -->
 <script src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
 
-<?php if(ENVIRONMENT === 'development' || ENVIRONMENT === 'testing') { ?>
+<?php if(ENVIRONMENT === 'development' || ENVIRONMENT === 'mediciones') { ?>
 	<!-- Estilos -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.css" />
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/fontawesome-all.css" />
